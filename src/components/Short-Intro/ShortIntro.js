c111928d14db1dc196acd967311e447d67196eb4
Vishnu <vishnusr01@gmail.com>
2021-06-18 13:58:45 +0800

    Modify intro include image

--- src/components/Short-Intro/ShortIntro.js
+++ src/components/Short-Intro/ShortIntro.js
@@ -1,13 +1,14 @@
 import '../../Animation.css';
+import './ShortIntro.css';
 
 function ShortIntro() {
     return (
         <section>
-            <div className="scroll name-section">
-                <p>
+            <div className="scroll name-section intro">
+                <div>
                     <div className="name fade-in-text-300">Hello, I am Vishnu S R</div>
                     <div className="name-fade fade-in-text-600">And I build microservice applications</div>
-                </p>
+                </div>
             </div>
         </section>
     );
