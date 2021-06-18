39c679f92d89efa75fd6f610f5c1f5df49108b8c
Vishnu <vishnusr01@gmail.com>
2021-06-18 15:25:33 +0800

    Add skills in about me section

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -8,8 +8,27 @@ function AboutMe() {
             <div className="scroll name-section">
                 <p className="widthh">
                     I am a Singapore based Software Engineer working at DBS Bank.
-                    I work on full stack and currently work on internal applications at DBS
+                    I work on full stack and currently working on internal applications at DBS
                 </p>
+
+                <p>Some of the technologies that I currently work with are</p>
+
+                <div className="skills">
+                    <div>
+                        <ul>
+                            <li>Kotlin</li>
+                            <li>Java</li>
+                            <li>Spring Boot</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul>
+                            <li>JavaScript</li>
+                            <li>MySQL</li>
+                            <li>PCF</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </section>
     );
