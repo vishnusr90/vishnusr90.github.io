21af6b1f85fcec8ca622ff3ce2c8f345d4ec48a5
Vishnu <vishnusr01@gmail.com>
2021-06-20 19:34:52 +0800

    Add picture tag

--- src/components/Short-Intro/ShortIntro.js
+++ src/components/Short-Intro/ShortIntro.js
@@ -7,10 +7,13 @@ function ShortIntro() {
             <div className="scroll name-section intro">
                 <div>
                     <div className="name fade-in-text-300">Hello, I am Vishnu S R</div>
-                    <div className="name-fade fade-in-text-600">And I build microservice applications</div>
+                    <div className="name-fade fade-in-text-600">And I build microservices</div>
                 </div>
-                <div id="my-pic">
-                    <img src="my-pic.jpeg" className="fade-in-text-300"/>
+                <div id="my-pic" className="fade-in-text-600">
+                    <picture>
+                        <source srcSet="./my-pic.jpeg" media="all and (min-width:800px) and (max-width:1200px)"/>
+                        <img src="my-pic.jpeg" alt="My Picture" />
+                    </picture>
                 </div>
             </div>
         </section>
