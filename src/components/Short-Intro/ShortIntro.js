32ecd850a356a0e5c5ce41d05efa855f405ac15b
Vishnu <vishnusr01@gmail.com>
2021-06-22 10:22:37 +0800

    Display content in flex for short intro

--- src/components/Short-Intro/ShortIntro.js
+++ src/components/Short-Intro/ShortIntro.js
@@ -4,12 +4,14 @@ import './ShortIntro.css';
 function ShortIntro() {
     return (
         <section id="short-intro">
-            <div className="scroll name-section intro">
-                <div>
-                    <div className="name fade-in-text-300">Hello, I am Vishnu S R</div>
-                    <div className="name-fade fade-in-text-600">And I build microservices</div>
+            <div className="row">
+                <div className="scroll name-section intro">
+                    <div>
+                        <div className="name fade-in-text-300">Hello, I am Vishnu S R</div>
+                        <div className="name-fade fade-in-text-600">And I build microservices</div>
+                    </div>
                 </div>
-                <div id="my-pic" className="fade-in-text-600">
+                <div id="my-pic" className="fade-in-text-300">
                     <picture>
                         <source srcSet="./my-pic.jpeg" media="all and (min-width:800px) and (max-width:1200px)"/>
                         <img src="my-pic.jpeg" alt="My Picture" />
